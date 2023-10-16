70e8dc39418ca0d484cd11edb53a79b52f20a63f
Pauline Brisset <paulinebrisset@protonmail.com>
2023-10-16 16:16:20 +0400

    [ADD] Inertia intra-clusters

--- main.r
+++ main.r
@@ -1,39 +1,38 @@
-# On considère les données Iris de Fisher
+# Charger la bibliothèque "stats" pour K-means
+library(stats)
+
 # Charger la bibliothèque "datasets", puis les données Iris
 library(datasets)
 data(iris)
 
 # 1. Retirer la colonne classe des données
-iris_data <- iris[, -5]  # Retirer la colonne 5, qui est "Species"
-# 2. Calculer la matrice du carré de la distance euclidienne entre les individu
+iris_data <- iris[, -5]  # Retirer la colonne 5 "Species"
+
+# 2. Calculer la matrice du carré de la distance euclidienne entre les individus
 euclidean_distance_matrix <- dist(iris_data, method = "euclidean")^2
 
-#3. Définir "silPartition" qui calcule l’indice silhouette d’une partition
+# 3. Définir "silPartition" qui calcule l’indice silhouette d’une partition
 silPartition <- function(data, partition) {
-  n <- length(data)
+  n <- nrow(data)
   silhouette_values <- numeric(n)
-
+  cat("Nombre n :", n, "\n")
   for (i in 1:n) {
     a_i <- mean_distance_within_cluster(data, partition, i)
     b_i <- mean_distance_between_clusters(data, partition, i)
-
     silhouette_values[i] <- silhouette_coefficient(a_i, b_i)
+    cat("Silhouette :", silhouette_values[i], "\n")
   }
 
-  partition_silhouette <- mean(silhouette_values)
+  partition_silhouette <- mean(silhouette_values, na.rm = TRUE)
   return(partition_silhouette)
 }
+
 mean_distance_within_cluster <- function(data, clusters, point_index) {
   cluster_index <- clusters[point_index]
-  cluster_points <- data[clusters == cluster_index, , drop = FALSE]
-  point <- data[point_index, , drop = FALSE]
-
-  if (nrow(cluster_points) > 1) {
-    distances <- sqrt(rowSums((cluster_points - point)^2))
-    return(mean(distances))
-  } else {
-    return(0)
-  }
+  cluster_points <- data[clusters == cluster_index, ]
+  point <- data[point_index, ]
+  distances <- sqrt(rowSums((cluster_points - point)^2))
+  return(mean(distances))
 }
 
 mean_distance_between_clusters <- function(data, clusters, point_index) {
@@ -43,8 +42,8 @@ mean_distance_between_clusters <- function(data, clusters, point_index) {
   min_distance <- Inf
 
   for (cluster in unique_clusters) {
-    cluster_points <- data[clusters == cluster, , drop = FALSE]
-    point <- data[point_index, , drop = FALSE]
+    cluster_points <- data[clusters == cluster, ]
+    point <- data[point_index, ]
     distances <- sqrt(rowSums((cluster_points - point)^2))
     distance <- mean(distances)
 
@@ -65,3 +64,32 @@ silhouette_coefficient <- function(a_i, b_i) {
     return(0)
   }
 }
+
+# 4. Partitionner l’ensemble des individus en 3 classes par le K-Means direct
+k <- 3
+kmeans_result <- kmeans(as.matrix(iris_data), centers = k)
+cluster_assignments <- kmeans_result$cluster
+
+#5. Appliquer à la partition obtenue à la question (4) la fonction « silPartition » définie à la question (3)
+silhouette_index <- silPartition(as.matrix(iris_data), cluster_assignments)
+cat("Indice silhouette de la partition K-means :", silhouette_index, "\n")
+
+#6. Définir une fonction nommée « inertieIntraPartition » qui calcule l’inertie intra-classe d’une partition donnée
+inertieIntraPartition <- function(data, partition) {
+  n <- nrow(data)
+  k <- max(partition)  # Nombre de clusters
+  inertia <- 0
+
+  for (cluster in 1:k) {
+    cluster_indices <- which(partition == cluster)
+    if (length(cluster_indices) > 0) {
+      cluster_data <- data[cluster_indices, ]
+      cluster_center <- colMeans(cluster_data)
+      distances <- sqrt(rowSums((cluster_data - cluster_center)^2))
+      inertia <- inertia + sum(distances^2)
+    }
+  }
+  return(inertia)
+}
+inertie <- inertieIntraPartition(iris_data, cluster_assignments)
+cat("Inertie intra-classe de la partition K-means :", inertie, "\n")
